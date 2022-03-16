1f8f7f5fda6b36602797c47367d64a5b255cfd30
K Raghav <63174390+kraaghavin@users.noreply.github.com>
2022-03-16 19:03:46 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>andamanEARTH</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/Google_Earth_icon.svg-2" />
       </Head>
 
       <main>
