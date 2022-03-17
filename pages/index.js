b711506534d511f06f3231965f83be3e408d4fd7
K Raghav <63174390+kraaghavin@users.noreply.github.com>
2022-03-17 14:58:45 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,13 @@ export default function Home() {
       </Head>
 
       <main>
+    <div id="myCarousel" class="carousel container slide">
+<div class="carousel-inner">
+        <div class="active item one"></div>
+        <div class="item two"></div>
+        <div class="item three"></div>
+</div>
+</div>
         <Header title="Re-exploring the Andamans" />
         <p className="description">
         </p>
